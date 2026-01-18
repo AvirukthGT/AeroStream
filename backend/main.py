d3d6402fca9d2169ab80b48cbc93bdfde5817050
avirukthgt <avirukth@gmail.com>
2026-01-18 23:25:23 +1100

    Fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 # Enable CORS (Allows your Vue app on port 5173 to talk to this API)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=[
+        "http://localhost:5173", 
+        "https://aero-stream-virid.vercel.app"
+    ], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
